262cb3f266cb77d55442943e4af29014736e9d69
Zé <jose.ramos@lightspeedhq.com>
2022-12-09 12:31:26 -0300

    feat: add card to web

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,8 +1,9 @@
-import { Button } from "ui";
+import { Button, Card } from "ui";
 
 export default function Web() {
   return (
     <div>
+      <Card />
       <h1>Web</h1>
       <Button />
     </div>
